f1d8e030bd0ee537737fa49b6c102a050d67ab07
apexalexr <40649001+apexalexr@users.noreply.github.com>
2021-01-06 18:53:10 -0600

    Added csv writing

--- app.js
+++ app.js
@@ -1,17 +1,25 @@
 const csv = require('csv-parser')
 const fs = require('fs')
 const results = [];
-const emailArray = [];
-fs.createReadStream('emails.csv')
-  .pipe(csv())
+const createCsvWriter = require('csv-writer').createObjectCsvWriter;
+const csvWriter = createCsvWriter({
+    path: 'file.csv',
+    header: [
+        {id: 'NEUSGN', title: 'NEUSGN'},
+        {id: 'NEEMLT', title: 'NEEMLT'}
+    ]
+});
+fs.createReadStream('emails')
+  .pipe(csv({separator:'\t'}))
   .on('data', (data) => results.push(data))
   .on('end', () => {
-    console.log(results);
+    console.log(results)
     results.forEach(line => {
-        let email = line.email;
+        let email = line.NEEMLT;
         let opcoRegex = '@idaho.sysco.com'
         let replaced
-        while(email.indexOf(opcoRegex)!=-1) {
+        if(email.indexOf(opcoRegex)!=-1) {
+           while(email.indexOf(opcoRegex)!=-1) {
             let endPosition = email.indexOf(opcoRegex)
             let commaPosition = email.substr(0,endPosition).lastIndexOf(',')
             let firstName;
@@ -29,10 +37,24 @@ fs.createReadStream('emails.csv')
             replaced = email.replace(lastName+'.'+firstName+'@idaho.sysco.com',replacement)
             email=replaced;
         }
-        line.email = replaced;
+        line.NEEMLT = replaced;
+      }
     })
-    console.log(results)
+    console.log(results);
+    csvWriter.writeRecords(results)       // returns a promise
+    .then(() => {
+        console.log('...Done');
+    });
   });
+
+
+ 
+// const records = [
+//     {name: 'Bob',  lang: 'French, English'},
+//     {name: 'Mary', lang: 'English'}
+// ];
+ 
+
 
 
 //original email from neemlt
